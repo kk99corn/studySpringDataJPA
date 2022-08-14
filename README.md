92673fc31aad47b3700d7dd691f390305afe8528
99 <hahahoho5915@gmail.com>
2022-08-14 19:14:22 +0900

    Member, Team 예제 도메인 모델 및 테스트코드 추가

--- README.md
+++ README.md
@@ -13,6 +13,12 @@
  
 ## Study Notes 
 ### 08.14 - 1일차 
- - Entity + @Setter: 기본적으로 Entity에 필드 Setter 추천하지 않음.
  - @Transactional: JPA CUD는 무조건 트랜잭션안에서 실행되야함.
  - 동일한 Transaction 안에서는 영속성 컨텐츠들의 동일성을 보장함.
+ - Lombok
+    - @Setter: 실무에서 가급적 Entity에 Setter는 사용하지 않기
+    - @NoArgsConstructor AccessLevel.PROTECTED: 기본 생성자 막고 싶은데, JPA 스팩상 PROTECTED로 열어두어야 함
+    - @ToString은 가급적 내부 필드만(연관관계 없는 필드만)
+ - JPA
+    - @ManyToOne fetch타입 LAZY(지연로딩)으로 변경(default: EAGER(즉시로딩))
+      - fetch타입 EAGER(즉시로딩)으로 사용시 실무에서 성능최적화 힘듦
--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,30 +1,45 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter // 실무에서는 entity에 기본적으로 @Setter 잘 사용하지 않음.
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
 	@Id
 	@GeneratedValue
+	@Column(name = "member_id")
 	private Long id;
 	private String username;
+	private int age;
 
-	/*
-	JPA 기본적으로 인자없는 생성자 필요
-	 */
-	protected Member() {
-	}
+	// ManyToOne fetch타입 LAZY(지연로딩)으로 변경(default: EAGER(즉시로딩))
+	// - fetch타입 즉시로딩으로 사용시 실무에서 성능최적화 힘듦
+	@ManyToOne(fetch = FetchType.LAZY)    // N:1 MEMBER:TEAM
+	@JoinColumn(name = "team_id")
+	private Team team;
 
 	public Member(String username) {
 		this.username = username;
+	}
+
+	public Member(String username, int age, Team team) {
+		this.username = username;
+		this.age = age;
+		if (team != null) {
+			changeTeam(team);
+		}
+
+	}
+
+	public void changeTeam(Team team) {
+		this.team = team;
+		team.getMembers().add(this);
 	}
 }
 
